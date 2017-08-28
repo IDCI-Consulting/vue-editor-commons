2bd8f6e203f39e7d813a9ba1f8b256eef4ca6029
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-08-28 11:45:36 +0200

    Add missing exported modules

--- entrypoint.js
+++ entrypoint.js
@@ -1,14 +1,19 @@
-import checkBoxComponent from './components/options/checkbox.vue';
-import choiceComponent   from './components/options/choice.vue';
-import integerComponent  from './components/options/integer.vue';
-import numberComponent   from './components/options/number.vue';
-import textComponent     from './components/options/text.vue';
-import textareaComponent from './components/options/textarea.vue';
-import modalComponent    from './components/modal.vue';
-import httpMixin         from './mixins/http.vue';
-import jsonOptionMixin   from './mixins/jsonOption.vue';
-import optionMixin       from './mixins/option.vue';
-import waitForItMixin    from './mixins/waitForIt.vue';
+import checkBoxComponent     from './components/options/checkbox.vue';
+import choiceComponent       from './components/options/choice.vue';
+import integerComponent      from './components/options/integer.vue';
+import numberComponent       from './components/options/number.vue';
+import textComponent         from './components/options/text.vue';
+import textareaComponent     from './components/options/textarea.vue';
+import modalComponent        from './components/modal.vue';
+import httpMixin             from './mixins/http.vue';
+import jsonOptionMixin       from './mixins/jsonOption.vue';
+import optionMixin           from './mixins/option.vue';
+import waitForItMixin        from './mixins/waitForIt.vue';
+import actions               from './store/actions.js';
+import getters               from './store/getters.js';
+import mutations             from './store/mutations.js';
+import JsonToTwigTransformer from './utils/JsonToTwigTransformer.js';
+import utils                 from './utils/utils.js';
 
 export {
   checkBoxComponent,
