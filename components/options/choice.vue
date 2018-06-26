64284f92f18952fbbb685f3ab80e84d017e50996
Brahim BOUKOUFALLAH <boukoufallah.brahim@gmail.com>
2018-06-26 16:38:04 +0200

    Add an empty option on choice type.

--- components/options/choice.vue
+++ components/options/choice.vue
@@ -11,6 +11,7 @@
           :required="option.options.required"
           v-model="data"
           :name="name">
+      <option disabled selected value></option>
       <option :value="key" v-for="(choice, key) in option.options.choices">{{ choice }}</option>
     </select>
   </div>
